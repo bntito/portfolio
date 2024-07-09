6662effd27b65fc383231489b092fa4b214542e8
Bruno Tito <bruno.nicolas.tito@gmail.com>
2024-07-09 09:13:09 -0300

    project 2

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,24 +1,51 @@
 import React from 'react';
 import Iot from '../assets/images/iot.png';
+import Crece from '../assets/images/crece.png';
 
 function Projects() {
   return (
-    <section id="projects" className="min-h-screen bg-white p-8">
-      <h2 className="text-3xl font-bold mb-4">Proyectos</h2>
+    <section id="projects" className="min-h-screen bg-white p-8 flex flex-col items-center">
+      <h2 className="text-3xl font-bold mb-4 text-center">Proyectos</h2>
 
-      <div className="mb-8">
+      <div className="mb-8 w-full md:w-2/3 lg:w-1/2 text-center">
         <h3 className="text-xl font-semibold mb-2">Proyecto: IoT Academy</h3>
-        <p className="mb-4"><a href='https://iotacademy.netlify.app/' target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">https://iotacademy.netlify.app/</a></p>
+        <p className="mb-4">
+          <a href='https://iotacademy.netlify.app/' target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
+            https://iotacademy.netlify.app/
+          </a>
+        </p>
         <p className="mb-2">Descripción: Plataforma educativa para cursos interactivos.</p>
         <p className="mb-2">Base de datos: MySQL</p>
-        <p className="mb-4">Servidor: <a href="https://academyiotserver.onrender.com/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">https://academyiotserver.onrender.com/</a></p>
+        <p className="mb-4">
+          Servidor <br />
+          <a href="https://academyiotserver.onrender.com/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
+            https://academyiotserver.onrender.com/
+          </a>
+        </p>
 
-        <a href="https://iotacademy.netlify.app/" target="_blank" rel="noopener noreferrer" className="block mb-4">
-          <img src={Iot} alt="Vista previa de IoT Academy" className="w-full md:max-w-md rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300" />
-        </a>
+        <div className="flex justify-center">
+          <a href="https://iotacademy.netlify.app/" target="_blank" rel="noopener noreferrer" className="block mb-4">
+            <img src={Iot} alt="Vista previa de IoT Academy" className="w-full md:max-w-md rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300" />
+          </a>
+        </div>
       </div>
 
-      
+      <div className="mb-8 w-full md:w-2/3 lg:w-1/2 text-center">
+        <h3 className="text-xl font-semibold mb-2">Proyecto: Crecer</h3>
+        <p className="mb-4">
+          <a href='https://crecer.netlify.app' target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
+            https://crecer.netlify.app
+          </a>
+        </p>
+        <p className="mb-2">Descripción: Aplicación de análisis de sueños diseñada para explorar el subconsciente y descubrir aspectos ocultos.</p>
+        <p className="mb-2">Base de datos: MongoDB</p>
+
+        <div className="flex justify-center">
+          <a href="https://crecer.netlify.app" target="_blank" rel="noopener noreferrer" className="block mb-4">
+            <img src={Crece} alt="Vista previa de Crecer" className="w-full md:max-w-md rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300" />
+          </a>
+        </div>
+      </div>
     </section>
   );
 }
